f0d2d8bc96d58f91920daa2cd2c9ef122cfeac2e
pegCalde <calderonpeg@gmail.com>
2021-01-02 16:30:05 +0100

    nav inter pages + retour accueil via icone

--- projetEnvWin/MathsPage.xaml.cs
+++ projetEnvWin/MathsPage.xaml.cs
@@ -51,14 +51,129 @@ namespace projetEnvWin
             gridEleve.Children.Add(afficheEleve);
         }
 
+        /*ICONE MAISON POUR RETOURNER A L'ACCUEIL*/
+        private void ReturnHome_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), currentStudent);
+        }
+
+        /*RETOUR A L'ACCUEIL DANS MENU*/
+
         private void Home_Tapped(object sender, TappedRoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(MainPage), currentStudent);
         }
 
+        /*PARTIES MATHS DANS MENU*/
+        private void btnMgeo_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(GeometriePage), currentStudent);
+            }
+        }
+        private void btnMcalcul_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(CalculPage), currentStudent);
+            }
+        }
+        private void btnMmesures_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(MesuresPage), currentStudent);
+            }
+        }
+        private void btnMnum_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(NumerationPage), currentStudent);
+            }
+        }
+
+        /*SECTION HISTOIRE DANS MENU + SUR ACCUEIL*/
         private void btnHistoire_Tapped(object sender, TappedRoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(HistoirePage), currentStudent);
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(HistoirePage), currentStudent);
+            }
+        }
+        /*PARTIES HISTOIRE DANS MENU*/
+        private void btnHma_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(MoyenAgePage), currentStudent);
+            }
+        }
+        private void btnHtm_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(TpsModernePage), currentStudent);
+            }
+        }
+        private void btnHec_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(EpoqueContemporainePage), currentStudent);
+            }
+        }
+        private void btnHp_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(PrehistoirePage), currentStudent);
+            }
+        }
+
+        /*SECTION FRANCAIS DANS MENU + SUR ACCUEIL*/
+        private void btnFrancais_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(FrancaisPage), currentStudent);
+            }
+        }
+        /*PARTIES FRANCAIS DANS MENU*/
+        private void btnFrConjug_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(ConjugaisonPage), currentStudent);
+            }
+        }
+        private void btnFrLexique_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(LexiquePage), currentStudent);
+            }
+        }
+        private void btnFrGram_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(GrammairePage), currentStudent);
+            }
+        }
+        private void btnFrOrtho_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (currentStudent != null)
+            {
+                this.Frame.Navigate(typeof(OrthographePage), currentStudent);
+            }
+        }
+
+        /*PAGE AIDE DANS MENU*/
+        private void btnHelp_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AidePage));
         }
     }
 }
