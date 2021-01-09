8d6ad5939432aeb69efd42e817ca75dcaf3bab81
GuillaumeSavin <Guisav1996@hotmail.fr>
2021-01-09 23:19:00 +0100

    Add generation of question and correction

--- projetEnvWin/MathsNumPage.xaml.cs
+++ projetEnvWin/MathsNumPage.xaml.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml.Controls;
+using projetEnvWin.Données;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
@@ -24,9 +28,91 @@ namespace projetEnvWin
     public sealed partial class MathsNumPage : Page
     {
         Eleve currentStudent;
+        Matiere maths = new Matiere(new List<Partie>(), "Maths");
         public MathsNumPage()
         {
             this.InitializeComponent();
+            generateQuestions();
+        }
+
+        public void generateQuestions()
+        {
+            fillMathsPartie(15, 5, "Numération");
+            Partie num = maths.ListePartie[0];
+            Grid tmp = new Grid();
+
+
+            foreach (Question q in num.ListeQuestion)
+            {
+                RowDefinition questRow = new RowDefinition();
+                questRow.Height = new GridLength(1.0, GridUnitType.Star);
+                form.RowDefinitions.Add(questRow);
+                tmp = generateQuestion(q);
+                Grid.SetRow(tmp, form.RowDefinitions.Count - 1);
+                form.Children.Add(tmp);
+            }
+        }
+
+        public Grid generateQuestion(Question question)
+        {
+            StackPanel firstPanel = new StackPanel();
+            Grid container = new Grid();
+
+            RowDefinition firstRow = new RowDefinition();
+            firstRow.Height = new GridLength(1.0, GridUnitType.Star);
+            RowDefinition secondRow = new RowDefinition();
+            secondRow.Height = new GridLength(1.0, GridUnitType.Star);
+
+            container.RowDefinitions.Add(firstRow);
+            container.RowDefinitions.Add(secondRow);
+
+            List<RadioButton> test = new List<RadioButton>();
+            TextBlock enonce = new TextBlock();
+            enonce.Text = question.Enonce;
+            enonce.Foreground = new SolidColorBrush(Colors.White);
+            enonce.FontWeight = Windows.UI.Text.FontWeights.Bold;
+            container.Children.Add(enonce);
+            Grid.SetRow(enonce, 0);
+            firstPanel.Orientation = Orientation.Horizontal;
+            firstPanel.Background = new SolidColorBrush(Colors.DarkBlue);
+
+
+            foreach (Reponse r in question.ReponsesPossibles)
+            {
+                if (question.Types == Question.Type.CLOSE)
+                {
+                    RadioButton tmp = new RadioButton();
+                    tmp.Content = r.Contenu;
+                    tmp.Foreground = new SolidColorBrush(Colors.White);
+                    tmp.FontWeight = Windows.UI.Text.FontWeights.Bold;
+                    firstPanel.Children.Add(tmp);
+
+                }
+                else if (question.Types == Question.Type.QCM)
+                {
+                    CheckBox tmp = new CheckBox();
+                    tmp.Content = r.Contenu;
+                    firstPanel.Children.Add(tmp);
+                }
+            }
+            Grid.SetRow(firstPanel, 1);
+            container.Children.Add(firstPanel);
+
+            return container;
+        }
+
+        public void fillMathsPartie(int index, int length, string nom)
+        {
+            QuestionsMaths QMaths = new QuestionsMaths();
+            List<Question> lQuest = new List<Question>();
+
+            for (int i = index; i < index + length; i++)
+            {
+                lQuest.Add(QMaths[i]);
+            }
+
+            Partie mathsPart = new Partie(lQuest, nom);
+            maths.ListePartie.Add(mathsPart);
         }
 
         /*fait par Peggy*/
@@ -207,6 +293,52 @@ namespace projetEnvWin
             }
 
             return false;
+        }
+
+        private void btnValider_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Grid g in form.Children)
+            {
+                TextBlock tmp = g.Children[0] as TextBlock;
+                StackPanel tmp2 = g.Children[1] as StackPanel;
+                Question Qattributed = maths.ListePartie[0][tmp.Text];
+                for (int i = 0; i < tmp2.Children.Count; i++)
+                {
+
+                    if (tmp2.Children[i].GetType() == typeof(CheckBox))
+                    {
+                        CheckBox element = tmp2.Children[i] as CheckBox;
+                        string content = (string)element.Content;
+                        Reponse Rattributed = Qattributed[content];
+
+                        if (Rattributed.Statut == 1)
+                        {
+                            element.Background = new SolidColorBrush(Colors.Green);
+                        }
+                        else
+                        {
+                            element.Background = new SolidColorBrush(Colors.Red);
+                        }
+                    }
+                    else
+                    {
+                        RadioButton element = tmp2.Children[i] as RadioButton;
+                        string content = (string)element.Content;
+                        Reponse Rattributed = Qattributed[content];
+
+                        if (Rattributed.Statut == 1)
+                        {
+                            element.Background = new SolidColorBrush(Colors.Green);
+                        }
+                        else
+                        {
+                            element.Background = new SolidColorBrush(Colors.Red);
+                        }
+                    }
+
+
+                }
+            }
         }
         /* /fait par Peggy*/
 
